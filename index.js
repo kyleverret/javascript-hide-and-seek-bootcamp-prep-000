7d4062f8c76f08b57259513696b35a5734eebe95
Kyle Verret <kyleverret@yahoo.com>
2018-05-25 10:01:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested div.target');
+}
+
+function increaseRankBy(n){
+   var myVar = document.querySelectorAll('ul.ranked-list li');
+for (let i = 0; i < myVar.length; i++) {
+  myVar[i].innerHTML = (parseInt(myVar[i].innerHTML) + n).toString();
+}
+}
+
+function deepestChild(){
+  
+  function drill(current){
+    var myRes;
+    if (current.querySelector('div') != null){
+      current = current.querySelector('div');
+      return drill(current);
+  }
+    else{
+      return current;
+    }
+  
+}
+  
+  var current = document.getElementById('grand-node');
+  return drill(current);
+}
